184e586ac1fa6917635d701dba75008059da4256
James Brown <jamesraymondbrown@gmail.com>
2022-10-28 23:36:18 +0000

    Adding the odd.js exercise to my git

---
+++ odd.js
@@ -0,0 +1,9 @@
+function isOdd(x) {
+  if (x % 2 == 0) {
+    return "True"
+  } else { return "False"
+  }
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8)); 
---
+++ whichSchool.js
@@ -0,0 +1,15 @@
+let age = ""
+let school = ""
+
+const whichSchool = function(age) {
+  if (age < 13) {
+    return "Elementary School";
+  } else if (age >= 13 && age <= 18) {
+    return "Secondary School";
+  } else {
+    return "Lighthouse Labs";
+  }
+}
+
+console.log(whichSchool(24));
+
